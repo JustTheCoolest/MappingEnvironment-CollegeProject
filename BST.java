24011043889744d4f6d6a3d8642cfa36f880fd62
Andhavarapu Balu <74148176+JustTheCoolest@users.noreply.github.com>
2023-11-21 20:59:24 +0530

    Map print

--- BST.java
+++ BST.java
@@ -1,5 +1,6 @@
 public class BST {
     SearchNode root;
+    public int max_x=-1, max_y=-1, min_x=1, min_y=1; // Caution: Will be broken if custom start provided or deletion provided
 
     public BST() {
         this.root = null;
@@ -7,6 +8,18 @@ public class BST {
 
     public void insert(Node node) {
         root = insert(root, node);
+        if (node.x > max_x) {
+            max_x = node.x;
+        }
+        if (node.y > max_y) {
+            max_y = node.y;
+        }
+        if (node.x < min_x) {
+            min_x = node.x;
+        }
+        if (node.y < min_y) {
+            min_y = node.y;
+        }
     }
     private SearchNode insert(SearchNode root, Node node) {
         if (root == null) {
--- Map.java
+++ Map.java
@@ -3,8 +3,27 @@ public class Map {
     public Map() {
         nodes = new BST();
     }
+    public void show() {
+        System.out.println("Map:");
+        int max_x = nodes.max_x;
+        int max_y = nodes.max_y;
+        int min_x = nodes.min_x;
+        int min_y = nodes.min_y;
+        for (int i = min_y; i <= max_y; ++i) {
+            for (int j = min_x; j <= max_x; ++j) {
+                Node current = nodes.search(new int[] {j, i});
+                if (current == null) {
+                    System.out.print("0 ");
+                } else {
+                    System.out.print(Color.toString(current.data) + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
     // Task : Switch to a previous_node as parameter system instead of coordinates as parameter for better efficiency
     void move(int direction, int[] coordinates) {
+        show();
         Node current = nodes.search(coordinates);
         switch (direction) {
             case 0: // Move up
